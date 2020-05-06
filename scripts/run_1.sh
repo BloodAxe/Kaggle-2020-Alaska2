15d5a1b6fef22aa1f9b08a886ec13e98290d2e92
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-06 17:46:43 +0300

    Fix dataset & augs

--- scripts/run_1.sh
+++ scripts/run_1.sh
@@ -1,15 +1,15 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 CUDA_VISIBLE_DEVICES=0 python train.py -m rgb_resnet34 -b 96 -w 16 -d 0.2 -s cos --epochs 50 --fine-tune 10 --show -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 0 --seed 12340 --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 0 --seed 12340 --fp16 --balance
 
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 CUDA_VISIBLE_DEVICES=1 python train.py -m rgb_resnet34 -b 96 -w 16 -d 0.2 -s cos --epochs 50 --fine-tune 10 --show -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 1 --seed 12341 --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 1 --seed 12341 --fp16 --balance
 
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 CUDA_VISIBLE_DEVICES=2 python train.py -m rgb_resnet34 -b 96 -w 16 -d 0.2 -s cos --epochs 50 --fine-tune 10 --show -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 2 --seed 12342 --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 2 --seed 12342 --fp16 --balance
 
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 CUDA_VISIBLE_DEVICES=3 python train.py -m rgb_resnet34 -b 96 -w 16 -d 0.2 -s cos --epochs 50 --fine-tune 10 --show -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 3 --seed 12343 --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 1e-3 --fold 3 --seed 12343 --fp16 --balance
