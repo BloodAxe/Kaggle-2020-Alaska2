ff72bbc96ff86918ad72c3c3a5beee328a5e1da9
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-07 14:37:50 +0300

    Fix mixxing 255

--- scripts/run_2.sh
+++ scripts/run_2.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 python train.py -m rgb_resnet34 -b 184 -w 16 -d 0.2 -s cos --epochs 100 --fine-tune 10 -a safe\
   --modification-flag-loss bce 1 --modification-type-loss ce 0.1 --embedding-loss ccos 1.0\
-  -v -lr 1e-4 --fold 0 --seed 12340 --balance\
+  -v -lr 1e-4 --fold 0 --seed 12340\
   -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/May07_12_52_rgb_resnet34_fold0/main/checkpoints/last.pth
