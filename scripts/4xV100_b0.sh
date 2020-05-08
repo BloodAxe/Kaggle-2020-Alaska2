3697fb0172d8d9330d85c6715ebe227c97f4a3ea
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-08 16:35:25 +0300

    Add experimental cosine embedding loss

--- scripts/4xV100_b0.sh
+++ scripts/4xV100_b0.sh
@@ -1,4 +1,4 @@
-export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
+export KAGGLE_2020_ALASKA2=/data/alaska2
 
 python train.py -m rgb_b0 -b 184 -w 24 -d 0.2 -s simple --epochs 100 --fine-tune 10 -a light\
   --modification-flag-loss bce 1 --modification-type-loss ce 0.1 -v -lr 3e-4 --fold 0 --seed 12350
