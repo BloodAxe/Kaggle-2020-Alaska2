cddc63b02e3f5e8929edc98cb4441470bf2a196c
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-09 17:13:47 +0300

    Drop

---
+++ scripts/4xV100_densenet121.sh
@@ -0,0 +1,13 @@
+export KAGGLE_2020_ALASKA2=/data/alaska2
+
+python train2.py -m rgb_densenet121 --size 384 -b 192 -w 24 -d 0.2 -s cos --epochs 50 --fine-tune 10 -a safe\
+  --modification-flag-loss wbce 1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 0 --seed 0 -wd 1e-5
+
+python train2.py -m rgb_densenet121 --size 384 -b 192 -w 24 -d 0.2 -s cos --epochs 50 --fine-tune 10 -a safe\
+  --modification-flag-loss wbce 1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 1 --seed 1 -wd 1e-5
+
+python train2.py -m rgb_densenet121 --size 384 -b 192 -w 24 -d 0.2 -s cos --epochs 50 --fine-tune 10 -a safe\
+  --modification-flag-loss wbce 1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 2 --seed 2 -wd 1e-5
+
+python train2.py -m rgb_densenet121 --size 384 -b 192 -w 24 -d 0.2 -s cos --epochs 50 --fine-tune 10 -a safe\
+  --modification-flag-loss wbce 1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 3 --seed 3 -wd 1e-5
--- scripts/4xV100_srx50.sh
+++ scripts/4xV100_srx50.sh
@@ -7,7 +7,7 @@ python train2.py -m rgb_seresnext50 -b 16 -w 24 -d 0.5 -s cos --epochs 20 -a saf
   --modification-flag-loss wbce 1 --modification-type-loss ce 0.01 --embedding-loss cntr 1.0 -v -lr 1e-4 --fold 0 --seed 22350
 
 python train2.py -m rgb_seresnext50 -b 16 -w 24 -d 0.5 -s cos --epochs 20 -a safe\
-  --modification-flag-loss wbce 0.01 --embedding-loss cntrv2 1.0 -v -lr 1e-4 --fold 0 --seed 22350
+  --modification-flag-loss bce 0.01 --modification-type-loss ce 0.01 --embedding-loss cntrv2 1.0 -v -lr 1e-4 --fold 0 --seed 22350
 
 
 python train2.py -m rgb_seresnext50 --size 384 -b 32 -w 24 -d 0.2 -s cos --epochs 100 --fine-tune 10 -a safe\
