5a7c40477060f76cafab1d18ace13678e0aab1c8
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-11 11:48:19 +0300

    Fix folds

--- scripts/4xV100_densenet121.sh
+++ scripts/4xV100_densenet121.sh
@@ -13,4 +13,4 @@ export KAGGLE_2020_ALASKA2=/data/alaska2
 #  --modification-flag-loss wbce2 1 --modification-type-loss ce 0.01 -v -lr 3e-4 --fold 3 --seed 3 -wd 1e-5 --fp16 > rgb_densenet201_fold3.log &
 
 nohup python train.py -m rgb_densenet201 -b 80 -w 24 -s cos --epochs 100 -a safe\
-  --modification-flag-loss bce1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 3 --seed 3 -wd 1e-5 --fp16 -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/May10_16_32_rgb_densenet201_fold3_fp16/main/checkpoints_auc/last.pth > rgb_densenet201_fold3.log &
+  --modification-flag-loss bce 1 --modification-type-loss ce 0.01 -v -lr 1e-4 --fold 3 --seed 3 -wd 1e-5 --fp16 -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/May10_16_32_rgb_densenet201_fold3_fp16/main/checkpoints_auc/last.pth > rgb_densenet201_fold3.log &
