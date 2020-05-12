03f09fe29031284eb9416dc2b6b336ac8cf34527
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-12 10:50:09 +0300

    flat_cos LR

--- alaska2/scheduler.py
+++ alaska2/scheduler.py
@@ -1,3 +1,6 @@
+import math
+import warnings
+
 from catalyst.contrib.schedulers import OneCycleLRWithWarmup
 from pytorch_toolbelt.optimization.lr_schedules import PolyLR
 from torch.optim import Optimizer
@@ -6,7 +9,9 @@ from torch.optim.lr_scheduler import (
     CyclicLR,
     MultiStepLR,
     CosineAnnealingLR,
-    CosineAnnealingWarmRestarts, LambdaLR,
+    CosineAnnealingWarmRestarts,
+    LambdaLR,
+    _LRScheduler,
 )
 
 
@@ -18,6 +23,80 @@ class PolyUpLR(LambdaLR):
         super().__init__(optimizer, poly_lr)
 
 
+class FlatCosineAnnealingLR(_LRScheduler):
+    r"""Set the learning rate of each parameter group using a cosine annealing
+    schedule, where :math:`\eta_{max}` is set to the initial lr and
+    :math:`T_{cur}` is the number of epochs since the last restart in SGDR:
+
+    .. math::
+        \eta_t = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1 +
+        \cos\left(\frac{T_{cur}}{T_{max}}\pi\right)\right)
+        T_{cur} \neq (2k+1)T_{max};\\
+        \eta_{t+1} = \eta_{t} + (\eta_{max} - \eta_{min})\frac{1 -
+        \cos(\frac{1}{T_{max}}\pi)}{2},
+        T_{cur} = (2k+1)T_{max}.\\
+
+    When last_epoch=-1, sets initial lr as lr. Notice that because the schedule
+    is defined recursively, the learning rate can be simultaneously modified
+    outside this scheduler by other operators. If the learning rate is set
+    solely by this scheduler, the learning rate at each step becomes:
+
+    .. math::
+        \eta_t = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1 +
+        \cos\left(\frac{T_{cur}}{T_{max}}\pi\right)\right)
+
+    It has been proposed in
+    `SGDR: Stochastic Gradient Descent with Warm Restarts`_. Note that this only
+    implements the cosine annealing part of SGDR, and not the restarts.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        T_max (int): Maximum number of iterations.
+        eta_min (float): Minimum learning rate. Default: 0.
+        last_epoch (int): The index of last epoch. Default: -1.
+
+    .. _SGDR\: Stochastic Gradient Descent with Warm Restarts:
+        https://arxiv.org/abs/1608.03983
+    """
+
+    def __init__(self, optimizer, T_max, T_flat, eta_min=0, last_epoch=-1):
+        self.T_max = T_max
+        self.T_flat = T_flat
+        self.eta_min = eta_min
+        super(FlatCosineAnnealingLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn(
+                "To get the last learning rate computed by the scheduler, " "please use `get_last_lr()`.",
+                DeprecationWarning,
+            )
+
+        if self.last_epoch == 0:
+            return self.base_lrs
+        elif (max(0, self.last_epoch - self.T_flat) - 1 - max(0, self.T_max - self.T_flat)) % (2 * max(0, self.T_max - self.T_flat)) == 0:
+            return [
+                group["lr"] + (base_lr - self.eta_min) * (1 - math.cos(math.pi / max(0, self.T_max - self.T_flat))) / 2
+                for base_lr, group in zip(self.base_lrs, self.optimizer.param_groups)
+            ]
+        return [
+            (1 + math.cos(math.pi * max(0, self.last_epoch - self.T_flat) / max(0, self.T_max - self.T_flat)))
+            / (1 + math.cos(math.pi * (max(0, self.last_epoch - self.T_flat) - 1) / max(0, self.T_max - self.T_flat)))
+            * (group["lr"] - self.eta_min)
+            + self.eta_min
+            for group in self.optimizer.param_groups
+        ]
+
+    def _get_closed_form_lr(self):
+        return [
+            self.eta_min
+            + (base_lr - self.eta_min)
+            * (1 + math.cos(math.pi * max(0, self.last_epoch - self.T_flat) / max(0, self.T_max - self.T_flat)))
+            / 2
+            for base_lr in self.base_lrs
+        ]
+
+
 def get_scheduler(scheduler_name: str, optimizer, lr, num_epochs, batches_in_epoch=None):
     if scheduler_name is None or scheduler_name.lower() == "none":
         return None
@@ -27,6 +106,9 @@ def get_scheduler(scheduler_name: str, optimizer, lr, num_epochs, batches_in_epo
 
     if scheduler_name.lower() == "cos":
         return CosineAnnealingLR(optimizer, num_epochs, eta_min=5e-5)
+
+    if scheduler_name.lower() == "flat_cos":
+        return FlatCosineAnnealingLR(optimizer, num_epochs, int(num_epochs * 0.8), eta_min=5e-5)
 
     if scheduler_name.lower() == "cosr":
         return CosineAnnealingWarmRestarts(optimizer, T_0=max(2, num_epochs // 10), eta_min=5e-5)
