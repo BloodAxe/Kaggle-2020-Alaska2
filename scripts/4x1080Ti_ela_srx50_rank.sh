30728a0e3a8cb1cad26c649645937098479d3261
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-20 23:23:13 +0300

    Script

--- scripts/4x1080Ti_ela_srx50_rank.sh
+++ scripts/4x1080Ti_ela_srx50_rank.sh
@@ -1,7 +1,4 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python train2.py -m ela_skresnext50_32x4d -b 38 -w 24 -d 0.2 -s flat_cos -o Ranger --epochs 10 -a light\
-  --modification-flag-loss rank2 1 -lr 1e-3 --fold 0 --seed 0 --fp16
-
-python train2.py -m ela_skresnext50_32x4d -b 38 -w 24 -d 0.2 -s flat_cos -o Ranger --epochs 10 -a light\
-  --modification-type-loss ce 1 -lr 1e-3 --fold 0 --seed 0 --fp16
+python train4.py -m ela_skresnext50_32x4d -b 19 -w 24 -d 0.2 -s flat_cos -o Ranger --epochs 25 -a light\
+  --modification-flag-loss rank2 1 -lr 1e-4 --fold 1 --seed 1 --fp16 -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/models/May15_17_03_ela_skresnext50_32x4d_fold1_fp16/best.pth
