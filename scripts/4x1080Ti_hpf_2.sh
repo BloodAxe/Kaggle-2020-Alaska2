d60688cc321346afec9a28651b4106134de7e55e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-23 00:07:45 +0300

    1

--- scripts/4x1080Ti_hpf_2.sh
+++ scripts/4x1080Ti_hpf_2.sh
@@ -1,6 +1,6 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python train.py -m hpf_net_v2 -b 96 -w 24 -d 0.2 -s flat_cos -o Ranger --epochs 25 -a light\
+python train.py -m hpf_net2 -b 96 -w 24 -d 0.2 -s flat_cos -o Ranger --epochs 25 -a light\
   --modification-flag-loss rank2 1\
   --modification-type-loss ce 0.1\
   -lr 1e-4 --fold 1 --seed 111 --fp16\
