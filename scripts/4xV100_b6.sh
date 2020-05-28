336d38e208856064b7b83b7e8d3ed880bf17d702
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-05-28 21:51:41 +0300

    B6

--- scripts/4xV100_b6.sh
+++ scripts/4xV100_b6.sh
@@ -1,4 +1,4 @@
 export KAGGLE_2020_ALASKA2=/data/alaska2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 14 -w 4 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light\
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 12 -w 4 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light\
   --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 --fold 1 --seed 1 -v --fp16
