3cbc6ab35c358d0ed350dd56b680ef20fb818451
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-04 12:40:57 +0300

    Script for DCT

--- scripts/4x1080Ti_dct_seresnext50.sh
+++ scripts/4x1080Ti_dct_seresnext50.sh
@@ -1,5 +1,6 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m dct_seresnext50 -b 64 -w 8 -d 0.25 -s flat_cos -o Ranger --epochs 75 -a light\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 --fold 0 --seed 1000 -v --fp16\
-  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/May31_15_04_dct_seresnext50_fold0_local_rank_0_fp16/main/checkpoints/best.pth
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m dct_seresnext50 -b 96 -w 7 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light --modification-type-loss ce 1 -lr 1e-3 --fold 0 --seed 0 -v --fp16 -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun04_11_34_dct_seresnext50_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/last.pth
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m dct_seresnext50 -b 96 -w 7 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light --modification-type-loss ce 1 -lr 1e-3 --fold 1 --seed 1 -v --fp16
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m dct_seresnext50 -b 96 -w 7 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light --modification-type-loss ce 1 -lr 1e-3 --fold 2 --seed 2 -v --fp16
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m dct_seresnext50 -b 96 -w 7 -d 0.2 -s flat_cos -o Ranger --epochs 75 -a light --modification-type-loss ce 1 -lr 1e-3 --fold 3 --seed 3 -v --fp16
