800281d6c6671f1c301a9f5db51588dd04e80b49
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-11 08:53:14 +0300

    Fold 2

--- scripts/4x1080Ti_b6.sh
+++ scripts/4x1080Ti_b6.sh
@@ -6,12 +6,12 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 #python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --warmup 10 --epochs 20 -a medium\
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 1 --seed 10001 -v --fp16
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 20 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 1 --seed 10001 -v --fp16 -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun09_11_45_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16/main/checkpoints/last.pth
+#python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 20 -a medium\
+#  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 1 --seed 10001 -v --fp16 -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun09_11_45_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16/main/checkpoints/last.pth
 
-#python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
-#  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
-#
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 50 -a medium\
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
+
 #python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 3 --seed 10003 -v --fp16
 
