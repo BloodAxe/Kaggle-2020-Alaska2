b2f6f4955eb8979cc3489568e8d4a7e00ef064ef
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-15 00:24:02 +0300

    Predictions

---
+++ scripts/infer_devcube.sh
@@ -0,0 +1,20 @@
+export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
+
+python oof_predictions.py\
+  models\Jun05_08_49_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16\main\checkpoints_auc_classifier\best.pth\
+  models\Jun05_08_49_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16\main\checkpoints_auc\best.pth\
+  models\Jun05_08_49_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16\main\checkpoints\best.pth\
+  \
+  models\Jun09_16_38_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16\main\checkpoints_auc_classifier\best.pth\
+  models\Jun09_16_38_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16\main\checkpoints_auc\best.pth\
+  models\Jun09_16_38_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16\main\checkpoints\best.pth\
+  \
+  models\Jun11_08_51_rgb_tf_efficientnet_b6_ns_fold2_local_rank_0_fp16\main\checkpoints_auc_classifier\best.pth\
+  models\Jun11_08_51_rgb_tf_efficientnet_b6_ns_fold2_local_rank_0_fp16\main\checkpoints_auc\best.pth\
+  models\Jun11_08_51_rgb_tf_efficientnet_b6_ns_fold2_local_rank_0_fp16\main\checkpoints\best.pth\
+  \
+  models\Jun11_18_38_rgb_tf_efficientnet_b6_ns_fold3_local_rank_0_fp16\main\checkpoints_auc_classifier\best.pth\
+  models\Jun11_18_38_rgb_tf_efficientnet_b6_ns_fold3_local_rank_0_fp16\main\checkpoints_auc\best.pth\
+  models\Jun11_18_38_rgb_tf_efficientnet_b6_ns_fold3_local_rank_0_fp16\main\checkpoints\best.pth\
+  \
+  -b 64 -w 4 -f -d4 -hv
