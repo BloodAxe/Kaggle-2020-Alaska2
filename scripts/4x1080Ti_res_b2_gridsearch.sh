56e4b5b2c5fc01bea2e9f708ad781ac609d23621
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-16 16:08:09 +0300

    Add residual input

--- scripts/4x1080Ti_res_b2_gridsearch.sh
+++ scripts/4x1080Ti_res_b2_gridsearch.sh
@@ -6,8 +6,8 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
 # RGB + residual input
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_res_tf_efficientnet_b2_ns -b 24 -w 8 -d 0.2 -s cos -o SGD --epochs 50 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 --fold 0 --seed 10000 -v --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16
 
 # RGB + residual with late concat
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_res_sms_tf_efficientnet_b2_ns -b 12 -w 8 -d 0.2 -s cos -o SGD --epochs 50 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 --fold 0 --seed 10000 -v --fp16
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16
