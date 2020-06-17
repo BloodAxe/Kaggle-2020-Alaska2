9c71f5e7b0fb7357ca0ab185fc4f42d9a6fc4b89
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-17 22:07:28 +0300

    b 14

--- scripts/4x1080Ti_res_b2_gridsearch.sh
+++ scripts/4x1080Ti_res_b2_gridsearch.sh
@@ -9,5 +9,5 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16
 
 # RGB + residual with late concat
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_res_sms_tf_efficientnet_b2_ns -b 12 -w 8 -d 0.2 -s cos -o RAdam --epochs 50 -a medium\
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_res_sms_tf_efficientnet_b2_ns -b 14 -w 8 -d 0.2 -s cos -o RAdam --epochs 50 -a medium\
   --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16
