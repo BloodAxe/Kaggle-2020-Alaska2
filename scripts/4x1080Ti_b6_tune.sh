17c48346d6c879198acd0ca7271c7886b51f2b1c
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-18 19:23:37 +0300

    b6 tune

---
+++ scripts/4x1080Ti_b6_tune.sh
@@ -0,0 +1,6 @@
+export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
+
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s flat_cos -o SGD --epochs 50 -a medium\
+  --modification-flag-loss wbce 1 --modification-type-loss focal 1 -lr 1e-3 --fold 0 --seed 100 -v --fp16\
+  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/models/Jun05_08_49_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16/main/checkpoints/best.pth
+
