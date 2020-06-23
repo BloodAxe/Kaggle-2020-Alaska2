2812e662169dca76a70608792391a7a3e3793cdf
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-23 12:05:17 +0300

    U

--- scripts/4x1080Ti_res_b2_gridsearch.sh
+++ scripts/4x1080Ti_res_b2_gridsearch.sh
@@ -15,5 +15,5 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_res_tf_efficientnet_b2_ns -b 24 -w 8 -d 0.2 -s cos -o SGD --epochs 50 -a hard\
   --modification-flag-loss wbce 1 --modification-type-loss focal 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16\
-  --c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun16_16_10_rgb_res_tf_efficientnet_b2_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
+  --checkoint /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun16_16_10_rgb_res_tf_efficientnet_b2_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
 
