1cef5fee0faed62ebf3a4ccf2a4822cf225f79db
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-24 13:27:39 +0300

    ela_b2

---
+++ scripts/4x1080Ti_ela_b2.sh
@@ -0,0 +1,4 @@
+export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
+
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m ela_tf_efficientnet_b2_ns -b 24 -w 8 -d 0.2 -s cos -o SGD --epochs 50 -a hard\
+  --modification-flag-loss wbce 1 --modification-type-loss focal 1 -lr 1e-2 -wd 1e-5 --fold 0 --seed 10000 -v --fp16
