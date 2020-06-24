bdda9efcb87806fb9dfefa79848eaf5ad87dcba7
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-24 21:05:01 +0300

    Fix?

--- scripts/4x1080Ti_rgb_b2.sh
+++ scripts/4x1080Ti_rgb_b2.sh
@@ -1,7 +1,7 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
-  -m rgb_tf_efficientnet_b2 -b 28 -w 8 -d 0.25 -s cos -o SGD --epochs 50 -a medium\
+  -m rgb_tf_efficientnet_b2_ns -b 28 -w 8 -d 0.25 -s cos -o SGD --epochs 50 -a medium\
   --modification-flag-loss rank2 1 --modification-type-loss roc_auc_ce 1\
   -lr 1e-2 -wd 1e-5 --fold 2 --seed 10000 -v --fp16\
   -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun02_12_26_rgb_tf_efficientnet_b2_ns_fold2_local_rank_0_fp16/Jun02_12_26_rgb_tf_efficientnet_b2_ns_fold2_local_rank_0_fp16.pth
