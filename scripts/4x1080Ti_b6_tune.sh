19393c37a4f020fdd8a1c3fac5e9ee3a12206d09
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-26 17:33:46 +0300

    Script to train siamese

--- scripts/4x1080Ti_b6_tune.sh
+++ scripts/4x1080Ti_b6_tune.sh
@@ -7,24 +7,24 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
   -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 5e-3 -v --fp16\
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 -v --fp16\
   --fold 0 --seed 100\
   --checkpoint /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun05_08_49_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
   -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 5e-3 -v --fp16\
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 -v --fp16\
   --fold 1 --seed 101\
   --checkpoint /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun09_16_38_rgb_tf_efficientnet_b6_ns_fold1_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
   -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 5e-3 -v --fp16\
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 -v --fp16\
   --fold 2 --seed 102\
   --checkpoint /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun11_08_51_rgb_tf_efficientnet_b6_ns_fold2_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
   -m rgb_tf_efficientnet_b6_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 25 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 5e-3 -v --fp16\
+  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-3 -v --fp16\
   --fold 3 --seed 103\
   --checkpoint /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jun11_18_38_rgb_tf_efficientnet_b6_ns_fold3_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
