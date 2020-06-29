4b7988c8c546e07782f74182506dc53f37543609
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-29 20:38:15 +0300

    RAdam

--- scripts/4xV100_bit_rx152.sh
+++ scripts/4xV100_bit_rx152.sh
@@ -10,5 +10,5 @@ export KAGGLE_2020_ALASKA2=/data/alaska2
 #python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b7_ns -b 10 -w 6 -d 0.2 -s cos -o fused_sgd --epochs 50 -a medium\
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 30 -v --fp16 --obliterate 0.05
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m bit_m_rx152_2 -b 3 -w 6 -d 0.2 -s cos -o Ranger --epochs 50 -a hard\
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m bit_m_rx152_2 -b 3 -w 6 -d 0.2 -s cos -o RAdam --epochs 50 -a hard\
   --modification-flag-loss bce 1 --modification-type-loss ce 0.5 -lr 1e-3 --fold 3 --seed 40 -v --fp16
