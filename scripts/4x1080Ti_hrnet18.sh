281760340f52d42b711438729ce8cdc3c4683059
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-30 16:21:30 +0300

    hrnet18

--- scripts/4x1080Ti_hrnet18.sh
+++ scripts/4x1080Ti_hrnet18.sh
@@ -1,4 +1,4 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_hrnet18 -b 20 -w 6 -d 0.1 -s flat_cos -o RAdam --epochs 50 -a medium\
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_hrnet18 -b 28 -w 6 -d 0.1 -s flat_cos -o RAdam --epochs 50 -a medium\
   --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 3e-4 --fold 1 --seed 10000 -v --fp16
