6a9eb4d134587a156fa6b08e709ab5289f1ca6ba
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-06-30 23:11:49 +0300

    stride

--- scripts/4x1080Ti_hpf_net.sh
+++ scripts/4x1080Ti_hpf_net.sh
@@ -1,10 +1,10 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_net -b 28 -w 8 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
+#python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_net -b 26 -w 8 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
+#  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
+
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_b3_fixed_covpool -b 22 -w 4 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
   --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_b3_fixed_covpool -b 5 -w 4 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
-  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
-
-#python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_b3_fixed_gap -b 5 -w 4 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
+#python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m hpf_b3_fixed_gap -b 22 -w 4 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 2 --seed 10002 -v --fp16
