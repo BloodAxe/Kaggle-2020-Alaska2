1bb72d170f7d4097901a730f7912afbad2e10a9c
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-02 15:12:35 +0300

    Bitmix script

--- scripts/4x1080Ti_b2_bitmix.sh
+++ scripts/4x1080Ti_b2_bitmix.sh
@@ -1,4 +1,7 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b2_ns -b 8 -w 6 -d 0.2 -s cos -o SGD --epochs 75 -a medium\
-  --modification-flag-loss bce 1 -lr 1e-2 --fold 0 --seed 10000 -v --bitmix
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m bit_m_rx50_1 -b 12 -w 6 -d 0.2 -s cos -o SGD --epochs 75 -a light\
+  --modification-flag-loss bce 1 -lr 1e-2 --fold 0 -v --bitmix
+
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py -m rgb_tf_efficientnet_b2_ns -b 12 -w 6 -d 0.2 -s cos -o SGD --epochs 75 -a light\
+  --modification-flag-loss bce 1 -lr 1e-2 --fold 0 -v --bitmix
