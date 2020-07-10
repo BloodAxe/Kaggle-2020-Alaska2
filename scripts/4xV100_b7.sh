8f88f93bbc5ccc1ccf0e812ad91130ec3049a45d
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-10 17:34:20 +0300

    Fix B7 params

--- scripts/4xV100_b7.sh
+++ scripts/4xV100_b7.sh
@@ -14,7 +14,13 @@ export KAGGLE_2020_ALASKA2=/data/alaska2
 #  --modification-flag-loss bce 1 --modification-type-loss ce 1 -lr 1e-2 -wd 1e-4 --fold 3 --seed 40 -v --fp16 --obliterate 0.05
 
 
+#python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
+#  -m nr_rgb_tf_efficientnet_b7_ns_mish -b 10 -w 6 -s cos -o fused_sgd --epochs 50 -a light\
+#  --modification-flag-loss wbce 1 --modification-type-loss ce 1 -lr 1e-3 -wd 1e-4 --fold 1 --seed 20 -v --fp16\
+#  -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/Jun18_16_07_rgb_tf_efficientnet_b7_ns_fold1_local_rank_0_fp16/main/checkpoints/last.pth
+
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
-  -m nr_rgb_tf_efficientnet_b7_ns_mish -b 10 -w 6 -s cos -o fused_sgd --epochs 50 -a light\
-  --modification-flag-loss wbce 1 --modification-type-loss ce 1 -lr 1e-3 -wd 1e-4 --fold 1 --seed 20 -v --fp16\
-  -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/Jun18_16_07_rgb_tf_efficientnet_b7_ns_fold1_local_rank_0_fp16/main/checkpoints/last.pth
+  -m nr_rgb_tf_efficientnet_b7_ns_mish -b 10 -w 6 -s cos -o fused_sgd --epochs 50\
+  -a light --modification-flag-loss wbce 1 --modification-type-loss ce 1\
+  -lr 1e-3 -wd 1e-4 --fold 2 --seed 21110 -v --fp16\
+  -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/Jun20_09_52_rgb_tf_efficientnet_b7_ns_fold2_local_rank_0_fp16/main/checkpoints/last.pth
