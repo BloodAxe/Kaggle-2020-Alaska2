be9f7740ee7e495ea9a4785b4ede64981c17c8ac
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-11 19:31:59 +0300

    bits loss

--- scripts/4xV100_b7.sh
+++ scripts/4xV100_b7.sh
@@ -21,6 +21,6 @@ export KAGGLE_2020_ALASKA2=/data/alaska2
 
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
   -m nr_rgb_tf_efficientnet_b7_ns_mish -b 10 -w 6 -s cos -o fused_sgd --epochs 50\
-  -a light --modification-flag-loss wbce 1 --modification-type-loss ce 1\
-  -lr 1e-3 -wd 1e-4 --fold 2 --seed 21110 -v --fp16\
-  -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/Jun20_09_52_rgb_tf_efficientnet_b7_ns_fold2_local_rank_0_fp16/main/checkpoints/last.pth
+  -a hard --modification-flag-loss wbce 1 --modification-type-loss ce 1\
+  -lr 1e-4 -wd 1e-4 --fold 2 --seed 21110 -v --fp16\
+  -c /home/ubuntu/code/Kaggle-2020-Alaska2/runs/Jul10_14_45_nr_rgb_tf_efficientnet_b7_ns_mish_fold2_local_rank_0_fp16/main/checkpoints_auc_classifier/last.pth
