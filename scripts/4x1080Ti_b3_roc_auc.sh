52b902cc5bd11aeccb3f130cd2b87df4f23855d1
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-14 17:52:01 +0300

    nr_rgb_tf_efficientnet_b3_ns_mish

--- scripts/4x1080Ti_b3_roc_auc.sh
+++ scripts/4x1080Ti_b3_roc_auc.sh
@@ -1,6 +1,11 @@
 export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 
-python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
+#python -m torch.distributed.launch --nproc_per_node=4 train_d_paired.py\
+#  -m nr_rgb_tf_efficientnet_b3_ns_mish -b 20 -w 6 -s flat_cos -o adamw --epochs 50 -a medium\
+#  --bitmix --modification-flag-loss bce 1 --modification-type-loss roc_auc_ce 0.1 -lr 1e-4 -wd 1e-2 --fold 0 --seed 10000 -v --fp16\
+#  --transfer /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/F_Jun29_19_43_rgb_tf_efficientnet_b3_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
+
+python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
   -m nr_rgb_tf_efficientnet_b3_ns_mish -b 20 -w 6 -s flat_cos -o adamw --epochs 50 -a medium\
-  --bitmix --modification-flag-loss bce 1 --modification-type-loss roc_auc_ce 0.1 -lr 1e-4 -wd 1e-2 --fold 0 --seed 10000 -v --fp16\
+  --modification-flag-loss roc_auc 1 --modification-type-loss ce 0.1 -lr 1e-4 -wd 1e-2 --fold 0 --seed 10000 -v --fp16\
   --transfer /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/F_Jun29_19_43_rgb_tf_efficientnet_b3_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
