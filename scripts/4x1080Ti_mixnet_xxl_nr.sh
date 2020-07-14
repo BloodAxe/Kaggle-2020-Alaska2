a875059f4625dae7326897c80cc3f7a5359e5c21
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-14 11:02:45 +0300

    fine-tune with adamw

--- scripts/4x1080Ti_mixnet_xxl_nr.sh
+++ scripts/4x1080Ti_mixnet_xxl_nr.sh
@@ -12,8 +12,14 @@ export KAGGLE_2020_ALASKA2=/home/bloodaxe/datasets/ALASKA2
 #  --fold 1 --seed 101\
 #  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul08_13_05_nr_rgb_mixnet_xxl_fold1_paired_local_rank_0_fp16/main/checkpoints_auc_classifier/train.1.pth
 
+#python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
+#  -m nr_rgb_mixnet_xxl -b 10 -w 6 -s cos -o SGD --epochs 75 -a medium\
+#  --modification-flag-loss wbce 1 --modification-type-loss ce 1 -lr 8e-5 -wd 1e-5 -v --fp16\
+#  --fold 1 --seed 40101\
+#  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul09_23_37_nr_rgb_mixnet_xxl_fold1_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
+
 python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
-  -m nr_rgb_mixnet_xxl -b 10 -w 6 -s cos -o SGD --epochs 75 -a medium\
-  --modification-flag-loss wbce 1 --modification-type-loss ce 1 -lr 8e-5 -wd 1e-5 -v --fp16\
-  --fold 1 --seed 40101\
-  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul09_23_37_nr_rgb_mixnet_xxl_fold1_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
+  -m nr_rgb_mixnet_xxl -b 10 -w 6 -s flat_cos -o adamw --epochs 25 -a medium\
+  --modification-flag-loss wbce 1 --modification-type-loss ce 1 -lr 1e-4 -wd 1e-2 -v --fp16\
+  --fold 1 --seed 400101\
+  -c /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul13_22_34_nr_rgb_mixnet_xxl_fold1_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
