39cd8df9a19c7abf980e29eba2cb5537cf5d9441
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-07-15 06:19:22 +0300

    arcface script

--- blends/blend7/blend7.py
+++ blends/blend7/blend7.py
@@ -7,19 +7,30 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import plot_confusion_matrix
 
 v25_xl_NR_moreTTA_b4mish = pd.read_csv("submission_v25_xl_NR_moreTTA_b4mish.csv").sort_values(by="Id").reset_index()
+submission_v25_xl_NR_moreTTA_b4mish_b2mish_xlmish = (
+    pd.read_csv("submission_v25_xl_NR_moreTTA_b4mish_b2mish_xlmish.csv").sort_values(by="Id").reset_index()
+)
+submission_v26_dctr_jrm_srnet_mns_mnxlm_b2_b4m_b5m_srnetnopc70 = (
+    pd.read_csv("submission_v26_dctr_jrm_srnet_mns_mnxlm_b2_b4m_b5m_srnetnopc70.csv")
+    .sort_values(by="Id")
+    .reset_index()
+)
 
-mean_09406 = pd.read_csv("mean_0.9406_cls_Cf2cauc_Df1cauc_Df2cauc_Ff0cauc_Gf0cauc_Gf1cauc_Gf2cauc_Gf3cauc.csv")
 xgb_cls_gs_09445 = pd.read_csv(
-    "xgb_cls_gs_0.9445_Cf2cauc_Df1cauc_Df2cauc_Ff0cauc_Gf0cauc_Gf1cauc_Gf2cauc_Gf3cauc_.csv"
+    "xgb_cls_gs_0.9445_Cf2cauc_Df1cauc_Ff0cauc_Gf0cauc_Gf1cauc_Gf2cauc_Gf3cauc_Hnrmishf2cauc_.csv"
 )
 
 # Force 1.01 value of OOR values in my submission
 oor_mask = v25_xl_NR_moreTTA_b4mish.Label > 1.0
 
-mean_09406.loc[oor_mask, "Label"] = 1.01
 xgb_cls_gs_09445.loc[oor_mask, "Label"] = 1.01
 
-submissions = [v25_xl_NR_moreTTA_b4mish, v25_xl_NR_moreTTA_b4mish, mean_09406, xgb_cls_gs_09445]
+submissions = [
+    v25_xl_NR_moreTTA_b4mish,
+    submission_v25_xl_NR_moreTTA_b4mish_b2mish_xlmish,
+    submission_v26_dctr_jrm_srnet_mns_mnxlm_b2_b4m_b5m_srnetnopc70,
+    xgb_cls_gs_09445,
+]
 
 cm = np.zeros((len(submissions), len(submissions)))
 for i in range(len(submissions)):
@@ -28,13 +39,21 @@ for i in range(len(submissions)):
 
 print(cm)
 
-disp = ConfusionMatrixDisplay(
-    confusion_matrix=cm,
-    display_labels=["v25_xl_NR_moreTTA", "v25_xl_NR_moreTTA_b4mish", "mean_09406", "xgb_cls_gs_09445"],
-)
-plt.figure(figsize=(8, 8))
-disp.plot(include_values=True, cmap="Blues", ax=plt.gca(), xticks_rotation=45)
-plt.show()
+# disp = ConfusionMatrixDisplay(
+#     confusion_matrix=cm,
+#     display_labels=["v25_xl_NR_moreTTA", "v25_xl_NR_moreTTA_b4mish", "mean_09406", "xgb_cls_gs_09445"],
+# )
+# plt.figure(figsize=(8, 8))
+# disp.plot(include_values=True, cmap="Blues", ax=plt.gca(), xticks_rotation=45)
+# plt.show()
 
+# 939
+blend_6_ranked = blend_predictions_ranked([submission_v25_xl_NR_moreTTA_b4mish_b2mish_xlmish, xgb_cls_gs_09445])
+blend_6_ranked.to_csv("blend_7_ranked_v25_xl_NR_moreTTA_b4mish_b2mish_xlmish_with_xgb_cls_gs_09445.csv", index=False)
+
+#
 blend_6_ranked = blend_predictions_ranked([v25_xl_NR_moreTTA_b4mish, xgb_cls_gs_09445])
 blend_6_ranked.to_csv("blend_7_ranked_v25_xl_NR_moreTTA_b4mish_with_xgb_cls_gs_09445.csv", index=False)
+
+blend_6_ranked = blend_predictions_ranked([v25_xl_NR_moreTTA_b4mish, xgb_cls_gs_09445])
+blend_6_ranked.to_csv("blend_7_ranked_v26_v26_dctr_jrm_srnet_mns_mnxlm_b2_b4m_b5m_srnetnopc70_with_xgb_cls_gs_09445.csv", index=False)
--- scripts/4x1080Ti_b6_tune_nr_arc.sh
+++ scripts/4x1080Ti_b6_tune_nr_arc.sh
@@ -5,4 +5,4 @@ python -m torch.distributed.launch --nproc_per_node=4 train_d.py\
   -m nr_rgb_tf_efficientnet_b6_ns_mish_gep -b 8 -w 6 -s cos -o adamw --epochs 25 -a medium\
   --modification-flag-loss wbce 0.1 --embedding-loss arc_face 1 -lr 3e-4 -wd 1e-2 -v --fp16\
   --fold 0 --seed 100\
-  --transfer /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul03_21_14_nr_rgb_tf_efficientnet_b6_ns_fold0_local_rank_0_fp16/main/checkpoints_auc_classifier/best.pth
+  --transfer /home/bloodaxe/develop/Kaggle-2020-Alaska2/runs/Jul15_00_15_nr_rgb_tf_efficientnet_b6_ns_mish_gep_fold0_local_rank_0_fp16/main/checkpoints_auc_embedding/last.pth
